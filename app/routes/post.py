91f45e5e9939fd47a5c997c9d47170421e463b1e
Arundhati Das <arundhutidas2000@gmail.com>
2025-07-25 22:39:22 -0400

    refactor: enhance user retrieval in OAuth2 and post routes

--- app/routes/post.py
+++ app/routes/post.py
@@ -20,7 +20,7 @@ def get_posts(db: Session = Depends(get_db),get_current_user:int = Depends(oauth
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse)
 def create_posts(post: schemas.CreatePost,db: Session = Depends(get_db), get_current_user:int = Depends(oauth2.get_current_user)):
-    # print(get_current_user)
+    print(get_current_user)
     new_post=models.Post(**post.dict()) #unpacking the post dict to match the Post model
     db.add(new_post)
     db.commit()
@@ -59,7 +59,7 @@ def delete_post(id:int, db: Session = Depends(get_db)):
 
 
 @router.put("/{id}", status_code=status.HTTP_202_ACCEPTED, response_model=schemas.PostResponse)
-def update_post(id: int, updated_post: schemas.UpdatePost, db: Session = Depends(get_db)):
+def update_post(id: int, updated_post: schemas.UpdatePost, db: Session = Depends(get_db), get_current_user:int = Depends(oauth2.get_current_user)):
     post_query=db.query(models.Post).filter(models.Post.id == id)
     post= post_query.first()
     if post == None:
