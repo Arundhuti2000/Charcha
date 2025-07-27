0a8375ff6dbc1daa4edea76992f380b62c8a9f5a
Arundhati Das <arundhutidas2000@gmail.com>
2025-07-27 13:07:22 -0400

    feat: update database connection and OAuth2 settings to use configuration variables

--- app/oauth2.py
+++ app/oauth2.py
@@ -4,6 +4,7 @@ from . import schemas, database, models
 from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
+from .config import settings
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
@@ -12,9 +13,9 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 #Expiration_Time
 
 
-SECRET_KEY = "021385sd31vf53sd4azg1vfedr51vbf85rteg1vd53v1d5xv18saw3v1d3x2v123dx1sv5ws3av135sda5v1cx35v185sdv3a"
-ALGORITHM= "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+SECRET_KEY = settings.secret_key
+ALGORITHM= settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
 
 def create_access_token(data: dict):
     to_encode=data.copy()
